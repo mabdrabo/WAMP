62c799afb6161657a0343a10069e33613da909c9
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2013-10-24 15:15:47 +0200

    part setp func.

--- Where are my parts/src/backend/Part.java
+++ Where are my parts/src/backend/Part.java
@@ -2,7 +2,7 @@ package backend;
 
 import java.util.ArrayList;
 
-public class Part extends SearchTreeNode{
+public class Part {
 	
 	int[] location;
 	ArrayList<Part> linked_parts;
@@ -16,6 +16,42 @@ public class Part extends SearchTreeNode{
 	
 	public void linkPart(Part part) {
 		this.linked_parts.add(part);
+	}
+	
+	public void step(Grid grid, Operator direction) {
+		String tag = grid.grid[this.location[0]][this.location[1]];  
+		grid.grid[this.location[0]][this.location[1]] = "e";
+		switch (direction) {
+		
+		case NORTH:
+			this.location[0] = this.location[0] - 1;
+			for (Part part : linked_parts)
+				part.location[0] = part.location[0] - 1;
+			break;
+
+		case SOUTH:
+			this.location[0] = this.location[0] + 1;
+			for (Part part : linked_parts)
+				part.location[0] = part.location[0] + 1;
+			break;
+			
+		case EAST:
+			this.location[1] = this.location[1] + 1;
+			for (Part part : linked_parts)
+				part.location[1] = part.location[1] + 1;
+			break;
+			
+		case WEST:
+			this.location[1] = this.location[1] - 1;
+			for (Part part : linked_parts)
+				part.location[1] = part.location[1] - 1;
+			break;
+		}
+		grid.grid[this.location[0]][this.location[1]] = tag;
+	}
+	
+	public String toString() {
+		return String.format("i:%d j:%d", this.location[0], this.location[1]);
 	}
 
 }
