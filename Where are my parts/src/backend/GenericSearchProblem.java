2c35d60bf452d924716f793778cdc7b14377b1af
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2013-10-24 15:19:52 +0200

    refactoring and temp. hardcoding

--- Where are my parts/src/backend/GenericSearchProblem.java
+++ Where are my parts/src/backend/GenericSearchProblem.java
@@ -1,31 +1,16 @@
 package backend;
 
-enum SearchStrategy {
-	BREADTH_FIRST, DEPTH_FIRST, ITERATIVE_DEEPENING, GREEDY_1, GREEDY_2, A_STAR_1, A_STAR_2
-	};
-	
-enum Operator {
-	NORTH, SOUTH, EAST, WEST
-	};
+import java.util.Queue;
 
 public abstract class GenericSearchProblem {
 	
-	int operators;	// A set of operators, or actions, available to the agent
-	int[][] initial_state;
-	int[][] state_space;	// the set of states reachable from the initial state by any sequence of actions
+	enum operators {};	// A set of operators, or actions, available to the agent
+	SearchTreeNode initial_state;
+	Queue<SearchTreeNode> state_space;	// the set of states reachable from the initial state by any sequence of actions
 	
-	public GenericSearchProblem() {
-		
-	}
+	public abstract boolean goal_test(SearchTreeNode node);	//	the agent applies to a state to determine if it is a goal state.
 	
-	public boolean goal_test() {
-	//	the agent applies to a state to determine if it is a goal state
-		return false;
-	}
-	
-	public void path_cost() {
-	//	assigns cost to a sequence of actions.
+	public abstract void path_cost();	//	assigns cost to a sequence of actions.
 	//	Typically, it is the sum of the costs of individual actions in the sequence.
-	}
 
 }
--- Where are my parts/src/backend/Grid.java
+++ Where are my parts/src/backend/Grid.java
@@ -1,72 +1,75 @@
 package backend;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
-public class Grid {
+public class Grid extends SearchTreeNode{
 	//	Randomly generated rectangular grid of squares
 	
 	public ArrayList<Part> parts;
-	public char[][] grid;
+	public String[][] grid;
 	public int width;
 	public int height;
-	int min = 10;
-	int max = 25;
+	int min = 5;
+	int max = 10;
 	
 	public Grid() {
 		this.width = random(min, max);
 		this.height = random(min, max);
-		this.grid = new char[height][width];
+		this.grid = new String[height][width];
 		
 		System.out.println("width " + width + " height " + height);
 		for (int i=0; i<height; i++)
 			for (int j=0; j<width; j++)
 				if (i==0 || j==0 || i==height-1 || j==width-1) {
-					this.grid[i][j] = 'f';
+					this.grid[i][j] = "o";		// TODO CHANGE TO f INSTED OF o THIS WAS JUST A TEST
 				} else 
-					this.grid[i][j] = 'e';
+					this.grid[i][j] = "e";
 		
-		int obstacles_count = random(0, this.width*this.height/2);
+//		int obstacles_count = random(0, this.width*this.height/2);
+		int obstacles_count = 0;
+		System.out.println("#obstacles:" + obstacles_count);
 		for (int x=0; x<obstacles_count; x++) {
 			int i = random(0, this.height-1);
 			int j = random(0, this.width-1);
-			this.grid[i][j] = 'o';
+			this.grid[i][j] = "o";
 		}
 		
-		int parts_count = random(0, this.width*this.height/2);
+//		int parts_count = random(0, this.width*this.height/2);
+		int parts_count = 5;
+		System.out.println("#parts:" + parts_count);
 		parts = new ArrayList<Part>();
+		HashSet<Part> hashSetOfParts = new HashSet<>();	// used to remove duplicates while adding the randomly generated robotic parts
 		for (int x=0; x<parts_count; x++) {
 			int i = random(0, this.height-1);
 			int j = random(0, this.width-1);
-			this.grid[i][j] = 'p';
-			parts.add(new Part(i,j));
+			if (this.grid[i][j].contains("p"))
+				continue;
+			else this.grid[i][j] = "p" + x;
+			System.out.format("Part[%d][%d]\n", i, j);
+			hashSetOfParts.add(new Part(i,j));
 		}
+		parts_count = hashSetOfParts.size();
+		System.out.println("#parts (after removing dubs):" + parts_count);
+		parts.addAll(hashSetOfParts);
 		
-		for (int x=0; x<parts.size(); x++) {
-			for (int z=0; z<parts.size(); z++) {
-				Part p1 = parts.get(x);
-				Part p2 = parts.get(z);
-
-				if (p1.location[0] != p2.location[0]
-						&& p1.location[0] != p2.location[0]) {
-					if ((p1.location[0] == p2.location[0]) &&
-							(p1.location[1] == p2.location[1] + 1 || 
-							p1.location[1] == p2.location[1] - 1)) {
-						p1.linkPart(p2);
-						parts.remove(p2);
-					}
-					else
-						if ((p1.location[1] == p2.location[1]) &&
-								(p1.location[0] == p2.location[0] + 1 || 
-								p1.location[0] == p2.location[0] - 1)) {
-							p1.linkPart(p2);
-							parts.remove(p2);
-						}
-				}
-			}
-		}
 	}
+	
+	public Grid(Grid grid) {
+		this.parts = new ArrayList<Part>(grid.parts);
+		this.width = grid.width;
+		this.height = grid.height;
+		this.grid = new String[grid.height][grid.width];
+		this.grid = grid.grid;
+	}
+	
 	
 	public static int random(int min, int max) {
 		return min + (int)(Math.random() * ((max - min) + 1));
+	}
+	
+	public String toString() {
+		return Arrays.deepToString(this.grid);
 	}
 }
