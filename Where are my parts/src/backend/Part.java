14b0e57f839c474d8e637ec01017af668d82e623
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2013-11-01 15:06:08 +0200

    enhanced linkPart & toString, added hasPart, contains

--- Where are my parts/src/backend/Part.java
+++ Where are my parts/src/backend/Part.java
@@ -1,26 +1,48 @@
 package backend;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Part {
 	
-	int[] location;
-	ArrayList<int[]> linked_parts;
+	int id;
+	ArrayList<int[]> linked_parts_locations;
+	static int count = 0;
 
 	public Part(int i, int j) {
-		location = new int[2];
-		location[0] = i;
-		location[1] = j;
-		linked_parts = new ArrayList<int[]>();
-		linked_parts.add(new int[] {i, j});
+		this.id = count++;
+		linked_parts_locations = new ArrayList<int[]>();
+		linked_parts_locations.add(new int[] {i, j});
 	}
 	
-	public void linkPart(Part part) {
-		linked_parts.add(new int[] {part.location[0], part.location[1]});
+	public void linkPart(Grid grid, Part part) {
+		for (int[] loc : part.linked_parts_locations) {
+			if (!this.contains(loc)) {
+				System.out.println("Link success");
+				this.linked_parts_locations.add(loc);
+			}
+		}
+		grid.parts.remove(grid.parts.indexOf(part));
+	}
+	
+	public boolean hasPart(int[] location) {
+		for (int[] loc : this.linked_parts_locations)
+			if (loc[0] == location[0] && loc[1] == location[1])
+				return true;
+		return false;
 	}
 	
 	public String toString() {
-		return String.format("i:%d j:%d", this.location[0], this.location[1]);
+		return "P" + this.id + ": " + Arrays.deepToString(linked_parts_locations.toArray());
 	}
 
+	public boolean contains(Object o) {
+		int[] location = (int[]) o;
+		for (int[] loc : this.linked_parts_locations) {
+			if (loc[0] == location[0] && loc[1] == location[1])
+				return true;
+		}
+		return false;
+		
+	}
 }
