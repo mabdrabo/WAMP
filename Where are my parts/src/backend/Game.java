96be5a9c594984f86e84313cc9930e4aa30fdade
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2013-11-07 01:09:52 +0200

    ui enhancements

--- Where are my parts/src/backend/Game.java
+++ Where are my parts/src/backend/Game.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -16,20 +17,106 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
 public class Game {
 
 	private JFrame frmWhereAreMy;
+	private JPanel boardContainerPanel;
 	private JPanel boardPanel;
 	WhereAreMyParts backend;
 	Timer animator;
+	ArrayList<String[][]> goalPath;
 	int goalPathIndex = 0; 
-	
-	
+
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		Grid initial_grid = ((Grid) backend.initial_state);
+		frmWhereAreMy = new JFrame();
+		frmWhereAreMy.setTitle("Where are my parts?!");
+//		frmWhereAreMy.setResizable(false);
+//		frmWhereAreMy.setBounds(0, 0, (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+		frmWhereAreMy.setBounds(0, 0, 600, 600);
+		frmWhereAreMy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmWhereAreMy.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblStateName = new JLabel("Initial State");
+		lblStateName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStateName.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		frmWhereAreMy.getContentPane().add(lblStateName, BorderLayout.NORTH);
+		
+		JPanel controlPanel = new JPanel();
+		frmWhereAreMy.getContentPane().add(controlPanel, BorderLayout.WEST);
+		boardContainerPanel = new JPanel();
+		frmWhereAreMy.getContentPane().add(boardContainerPanel, BorderLayout.CENTER);
+		boardContainerPanel.setLayout(new BoxLayout(boardContainerPanel, BoxLayout.X_AXIS));
+		boardPanel = new JPanel(new GridLayout(backend.initial_state.state.length, backend.initial_state.state[0].length));
+		boardContainerPanel.add(boardPanel);
+		boardPanel.updateUI();
+		
+		JButton DFButton = new JButton("Depth First");
+		DFButton.setToolTipText("Apply Depth-first-search algorithm to solve problem");
+		DFButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SearchButton(SearchStrategy.DF);
+			}
+		});
+		
+		JButton BFButton = new JButton("Breadth First");
+		BFButton.setToolTipText("Apply Breadth-first-search algorithm to solve problem");
+		BFButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SearchButton(SearchStrategy.BF);
+			}
+		});
+		
+		JButton IDButton = new JButton("Iterative Deepening");
+		IDButton.setToolTipText("Apply Iterative-deepening algorithm to solve problem");
+		IDButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SearchButton(SearchStrategy.ID);
+			}
+		});
+		controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
+		
+		JLabel lblSearchStrategy = new JLabel("Search Strategy");
+		lblSearchStrategy.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSearchStrategy.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		controlPanel.add(lblSearchStrategy);
+		controlPanel.add(BFButton);
+		controlPanel.add(DFButton);
+		controlPanel.add(IDButton);
+		
+		JButton btnNewGame = new JButton("New Game");
+		btnNewGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				backend = new WhereAreMyParts();
+				paintBoard(backend.initial_state.state);
+			}
+		});
+		controlPanel.add(btnNewGame);
+		
+		JPanel statsPanel = new JPanel();
+		frmWhereAreMy.getContentPane().add(statsPanel, BorderLayout.EAST);
+		statsPanel.setLayout(new BoxLayout(statsPanel, BoxLayout.Y_AXIS));
+		
+		JLabel lblStat = new JLabel("Stats.");
+		lblStat.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		lblStat.setHorizontalAlignment(SwingConstants.CENTER);
+		statsPanel.add(lblStat);
+		
+		paintBoard(initial_grid.state);
+	}
+
 	public void paintBoard(String[][] gridState) {
-		frmWhereAreMy.getContentPane().remove(boardPanel);
-		boardPanel = new JPanel();
-		boardPanel.setLayout(new GridLayout(gridState.length, gridState[0].length, 0, 0));
-		frmWhereAreMy.getContentPane().add(boardPanel);
+		boardPanel.removeAll();
 		
 		for (int i=0; i<gridState.length; i++)
 			for (int j=0; j<gridState[0].length; j++) {
@@ -52,7 +139,8 @@ public class Game {
 							}
 				boardPanel.add(l);
 			}
-		boardPanel.repaint();
+		frmWhereAreMy.revalidate();
+		boardPanel.updateUI();
 	}
 	
 	public void SearchButton(SearchStrategy strategy) {
@@ -66,7 +154,7 @@ public class Game {
 		}
 		JOptionPane.showMessageDialog(frmWhereAreMy, msg, strategy.toString() + " stats", JOptionPane.INFORMATION_MESSAGE);
 		if (backend.goalNode != null) {
-			final ArrayList<String[][]> goalPath = new ArrayList<String[][]>();
+			goalPath = new ArrayList<String[][]>();
 			Grid node = backend.goalNode;
 			goalPath.add(0, node.state);
 			while (node.parent != null) {
@@ -74,21 +162,22 @@ public class Game {
 				System.out.println(node);
 				goalPath.add(0, node.state);
 			}
+			goalPath.add(0, node.state);
+
 			animator = new Timer(1000, null);
 			animator.setDelay(2000);
 			animator.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					if (goalPathIndex < goalPath.size()) {
-						System.out.println("Timer print");
-//						System.out.println(goalPath.get(goalPathIndex++));
 						paintBoard(goalPath.get(goalPathIndex++));
-						boardPanel.revalidate();
-						boardPanel.repaint();
+						boardPanel.updateUI();
+						boardPanel.validate();
 					}else {
-						boardPanel.revalidate();
-						boardPanel.repaint();
+						System.out.println("STOP TIMER");
 						animator.stop();
+						boardPanel.updateUI();
+						boardPanel.validate();
 					}
 				}
 			});
@@ -124,53 +213,5 @@ public class Game {
 		
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		Grid initial_grid = ((Grid) backend.initial_state);
-		frmWhereAreMy = new JFrame();
-		frmWhereAreMy.setTitle("Where are my parts?!");
-//		frmWhereAreMy.setResizable(false);
-		frmWhereAreMy.setBounds(100, 100, initial_grid.width * 20 + 200, (initial_grid.height * 20) + 222);
-		frmWhereAreMy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmWhereAreMy.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		boardPanel = new JPanel();
-		frmWhereAreMy.getContentPane().add(boardPanel);
-		boardPanel.setLayout(new GridLayout(initial_grid.height, initial_grid.width, 0, 0));
-		
-		JPanel controlPanel = new JPanel();
-		frmWhereAreMy.getContentPane().add(controlPanel);
-		
-		JButton DFButton = new JButton("Depth First");
-		DFButton.setToolTipText("Apply Depth-first-search algorithm to solve problem");
-		DFButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				SearchButton(SearchStrategy.DF);
-			}
-		});
-		
-		JButton BFButton = new JButton("Breadth First");
-		BFButton.setToolTipText("Apply Breadth-first-search algorithm to solve problem");
-		BFButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				SearchButton(SearchStrategy.BF);
-			}
-		});
-		
-		JButton IDButton = new JButton("Iterative Deepening");
-		IDButton.setToolTipText("Apply Iterative-deepening algorithm to solve problem");
-		IDButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				SearchButton(SearchStrategy.ID);
-			}
-		});
-		controlPanel.setLayout(new GridLayout(3, 2, 0, 0));
-		controlPanel.add(BFButton);
-		controlPanel.add(DFButton);
-		controlPanel.add(IDButton);
-		
-		paintBoard(initial_grid.state);
-	}
+	
 }
